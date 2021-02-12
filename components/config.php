d7e363ab07bc5ff292ac271b2904a393cc5c9940
redrum <samuelganter@gmail.com>
2021-02-12 17:34:45 -0500

    missed some stuff

--- components/config.php
+++ components/config.php
@@ -10,7 +10,5 @@ $site_fqdn = "FairShop.ca";
 $site_name_short = "FS.";
 $domain = "http://FairShop.ca";
 $paypal_clientid = "AYq_dPGy5F80jTOkuVpoVpxEfScvqhUg6SVXyTuz-yMViIiY14nRBkoPYzey-T0ptUEd9y6Um37aofSE";
-$paypal_secret = "EIyB8SkVSlCOMidzgzicL-bahK6hKVrk9A4q2dBORDQ5pkyhBRf_Vo_QiIikEJUw6To1gKS1aQRwny79";
-
 $geoloc_key = "be9d89b1c64f951f08c32c1d288a1640";
 ?>
